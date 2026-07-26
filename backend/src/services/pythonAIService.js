19539c7b7ef6bb4c019fb42ebd36b0cebffe8186
Archisman Kundu <archismankundu101@gmail.com>
2026-07-26 10:47:04 +0530

    Refactor analyzeSatellite function to simplify request

--- backend/src/services/pythonAIService.js
+++ backend/src/services/pythonAIService.js
@@ -1,18 +1,14 @@
 const axios = require("axios");
 
 const analyzeSatellite = async (lat, lng) => {
-
     const response = await axios.post(
         `${process.env.AI_SERVICE_URL}/analyze`,
-        {
-            lat,
-            lng
-        }
+        { lat, lng }
     );
 
     return response.data;
 };
 
 module.exports = {
-    analyzeSatellite
+    analyzeSatellite,
 };
