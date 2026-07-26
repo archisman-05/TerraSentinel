ac76eac7f7f31d51b2c4c548afc73af094024839
Archisman Kundu <archismankundu101@gmail.com>
2026-07-26 10:28:06 +0530

    Update AI service URL to use environment variable

--- backend/src/services/pythonAIService.js
+++ backend/src/services/pythonAIService.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const analyzeSatellite = async (lat, lng) => {
 
     const response = await axios.post(
-        "http://localhost:8000/analyze",
+        `${process.env.AI_SERVICE_URL}/analyze`,
         {
             lat,
             lng
